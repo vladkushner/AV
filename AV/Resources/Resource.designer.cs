f145e300db2eb71b81a1b526a8569403ad4074e3
Vlad <VladKushner@gmail.com>
2016-02-29 20:43:11 +0300

    Login layout changed

--- AV/Resources/Resource.designer.cs
+++ AV/Resources/Resource.designer.cs
@@ -61,7 +61,10 @@ namespace AV
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int Login = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
